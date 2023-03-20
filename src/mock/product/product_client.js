359c48a2e435a0262c7c2fa04ee2ac01a38a600d
MaJungHan <wjdgksak@naver.com>
2023-03-20 17:53:20 +0900

    처음부터시작

--- src/mock/product/product_client.js
+++
@@ -1,9 +0,0 @@
-class ProductClient {
-  fetchItems() {
-    return fetch("http://example.com/login/id+password").then((res) => {
-      res.json();
-    });
-  }
-}
-
-module.exports = ProductClient;
