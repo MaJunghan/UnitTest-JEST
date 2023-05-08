8f513f926391b1e6071891bacf94138e727b5bb0
MaJungHan <wjdgksak@naver.com>
2023-05-08 22:39:31 +0900

    add : good test coverage

---
+++ src/좋은테스트원칙/좋은테스트의커버리지.js
@@ -0,0 +1,9 @@
+// CORRECT
+
+/*
+  C: Conformance : 포맷을 준수 (전화번호, 이메일, 아이디)
+  O: Ordering : 순서 조건 확인하기 (순서가 중요한경우)
+  R: Range : 숫자의범위
+  R: Reference : ~일때, ~가 되었을때 특정한 상황에서 이런 동작을 한다.
+  E: Existence : 값이 존재하지 않을때, 어떻게동작 ?
+*/
