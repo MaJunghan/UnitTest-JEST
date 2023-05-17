39251ef161db6017c2362aacac21754871a59f20
MaJungHan <wjdgksak@naver.com>
2023-05-17 23:39:51 +0900

    test : stack

---
+++ src/stack/test/stack.test.js
@@ -0,0 +1,50 @@
+const Stack = require("../stack.js");
+
+describe("Stack", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("is created empty", () => {
+    expect(stack.size()).toBe(0);
+  });
+
+  it("allows to push item", () => {
+    stack.push("🍕");
+    expect(stack.size()).toBe(1);
+  });
+
+  describe("pop", () => {
+    it("throws an error if stack im emmpty", () => {
+      expect(() => {
+        stack.pop();
+      }).toThrow("stack is empty");
+    });
+
+    it("returns the last pushed item and removes it from the stack", () => {
+      stack.push("🍟");
+      stack.push("🍔");
+
+      expect(stack.pop()).toBe("🍔");
+      expect(stack.size()).toBe(1);
+    });
+  });
+
+  describe("peek", () => {
+    it("throws an error if stack im emmpty", () => {
+      expect(() => {
+        stack.peek();
+      }).toThrow("stack is empty");
+    });
+
+    it("returns the last pushed item but keeps it in the stack", () => {
+      stack.push("🍟");
+      stack.push("🍔");
+
+      expect(stack.peek()).toBe("🍔");
+      expect(stack.size()).toBe(2);
+    });
+  });
+});
